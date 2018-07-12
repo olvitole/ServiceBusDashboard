b963cc33f6ee6bc7c11f61bae662e2ab417736a8
Hossein Margani <hossein.margani@aqovia.com>
2018-07-12 15:43:47 +0100

    Changed the way we refresh the Service Bus page intervally

--- ServiceBusDashboard/Code/Config.cs
+++ ServiceBusDashboard/Code/Config.cs
@@ -12,5 +12,6 @@ namespace ServiceBusDashboard.Code
         public static string IgnoreTopics => ConfigurationManager.AppSettings["IgnoreTopics"];
         public static string IgnoreSubscriptions => ConfigurationManager.AppSettings["IgnoreSubscriptions"];
         public static string ExcludeSubscriptions => ConfigurationManager.AppSettings["ExcludeSubscriptions"];
+        public static int RefreshEvery => Convert.ToInt32(ConfigurationManager.AppSettings["RefreshEvery"]);
     }
 }
